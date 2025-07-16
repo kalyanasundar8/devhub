78bdaa192d383ecacf9a721610ecb56dc7079bfe
kalyanasundar8 <sundar31100@gmail.com>
2025-07-16 17:02:32 +0530

    Inputfield error cleared

--- components/atoms/InputField.tsx
+++ components/atoms/InputField.tsx
@@ -1,17 +1,25 @@
 "use client";
 import React from "react";
 import { Input } from "../ui/input";
+import { UseFormRegister } from "react-hook-form";
+
+type FormValues = {
+  username: string;
+  email: string;
+  password: string;
+  confirmpassword: string;
+};
 
 type Props = {
   type: string;
-  name: string;
-  register: any;
+  name: keyof FormValues;
+  register: UseFormRegister<FormValues>;
 };
 
 export default function InputField({ type, name, register }: Props) {
   return (
     <div>
-      <Input type={type} name={name} {...register(name)} />
+      <Input type={type} {...register(name)} />
     </div>
   );
 }
